ac6b624a321b8b9a452c8941bb72eb61e2eb2229
Akbar <akbarbadmus07@gmail.com>
2022-04-19 10:32:25 +0100

    project done 2

--- src/Components/Interest.js
+++ src/Components/Interest.js
@@ -3,7 +3,7 @@ export default function Interest() {
         <div className="col-7 container bg-light">
             <div className=" col-11 mx-auto py-4 ">
             <h3>Interest</h3>
-            <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+            <p>Food expert. Music scholar. Chess Player. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
             </div>
         </div>
     )
